091c9d6e1c8a74547ac6128899b87e1f0a52f51c
Snt <sntgaleano@gmail.com>
2024-09-17 10:35:08 -0500

    Modificaciones de admin y vista roles

---
+++ app/src/main/java/com/snt/aqualuxe/Admin/InicioAdmin.java
@@ -0,0 +1,61 @@
+package com.snt.aqualuxe.Admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.snt.aqualuxe.R;
+
+public class InicioAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_inicio_admin);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn = findViewById(R.id.btn_reservas);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para abrir la nueva actividad
+                Intent intent = new Intent(InicioAdmin.this, Reservas.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btn2 = findViewById(R.id.btn_empleados);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para abrir la nueva actividad
+                Intent intent = new Intent(InicioAdmin.this, Empleados.class);
+                startActivity(intent);
+            }
+        });
+
+
+        Button btn3 = findViewById(R.id.btn_autolavados);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Crear un Intent para abrir la nueva actividad
+                Intent intent = new Intent(InicioAdmin.this, Autolavados.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/snt/aqualuxe/Admin/ModificarReserva.java
+++ app/src/main/java/com/snt/aqualuxe/Admin/ModificarReserva.java
@@ -1,4 +1,4 @@
-package com.snt.aqualuxe.admin;
+package com.snt.aqualuxe.Admin;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.snt.aqualuxe.R;
 
-public class InicioAdmin extends AppCompatActivity {
+public class ModificarReserva extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_inicio_admin);
+        setContentView(R.layout.activity_modificar_reserva);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
